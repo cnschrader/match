0663688d76f67a372b1d3a91ec48c490090d9dde
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-07-04 10:24:20 -0500

    mapping over the image component, not working properly yet, but close.

--- match/src/App.jsx
+++ match/src/App.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import Header from './components/header';
+// import Header from './components/header';
 
 import './App.css';
 import Images from "./components/Cards/images";
-import pictures from '..src/images.json'
+import pictures from '../src/images.json'
 
 
 
 class App extends React.Component {
 
   state = {
-    
+    pictures,
+    score: 0,
+    bestScore: 0
   }
- 
 
 
-  render(){
+
+  render() {
     return (
-    <React.Fragment>
-      <Header />
-    
-<Images></Images>
-      
-    </React.Fragment>
+      <React.Fragment>
+        <React.Fragment>
+          <nav class="navbar navbar-light bg-light">
+            <span className="navbar-brand mb-0 h1">Score: {this.state.score}</span>
+            <span className="navbar-brand mb-0 h1"> Best Score: {this.state.bestScore}</span>
+          </nav>
+        </React.Fragment>
 
-    
-    
+        {this.state.pictures.map(picture => (
+          <Images
+          id={pictures.id}
+          image={pictures.image}
+           />
+        ))}
+
+      </React.Fragment>
+
+
+
     )
   }
 }
